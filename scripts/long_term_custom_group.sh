6a7477dfff536fb3aaf499bac952a34ad16821a9
Your Name <you@example.com>
2026-01-03 10:25:47 +0800

    add group 7

--- scripts/long_term_custom_group.sh
+++ scripts/long_term_custom_group.sh
@@ -1,13 +1,13 @@
 python3 run.py \
   --task_name long_term_forecast \
   --is_training 1 \
-  --model_id custom_group_6 \
+  --model_id custom_group_7 \
   --model TimesNet \
   --data custom \
   --root_path ./datasets \
-  --data_path adjusted_data_group_6.csv \
+  --data_path adjusted_data_group_7.csv \
   --features M \
-  --target 10LBA10CT103K \
+  --target 10CFA10GH001XQ41 \
   --freq s \
   --seq_len 12 \
   --label_len 12 \
@@ -15,7 +15,29 @@ python3 run.py \
   --enc_in 50 \
   --dec_in 50 \
   --c_out 50 \
-  --inverse\
+  --inverse \
+  --train_epochs 5 \
+  --batch_size 32 \
+  --devices 4 \
+
+python3 run.py \
+  --task_name long_term_forecast \
+  --is_training 0 \
+  --model_id custom_group_7 \
+  --model TimesNet \
+  --data custom \
+  --root_path ./datasets \
+  --data_path adjusted_data_group_7.csv \
+  --features M \
+  --target 10CFA10GH001XQ41 \
+  --freq s \
+  --seq_len 12 \
+  --label_len 12 \
+  --pred_len 1 \
+  --enc_in 50 \
+  --dec_in 50 \
+  --c_out 50 \
+  --inverse \
   --train_epochs 5 \
   --batch_size 32 \
   --devices 4 \
