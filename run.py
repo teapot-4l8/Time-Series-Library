46555725a63b82cd8a406f4fd05e6c7d9244c2b6
Your Name <you@example.com>
2026-01-03 12:10:37 +0800

    delete something

--- run.py
+++ run.py
@@ -177,20 +177,21 @@ if __name__ == '__main__':
     print('Args in experiment:')
     print_args(args)
 
-    if args.task_name == 'long_term_forecast':
-        Exp = Exp_Long_Term_Forecast
-    elif args.task_name == 'short_term_forecast':
-        Exp = Exp_Short_Term_Forecast
-    elif args.task_name == 'imputation':
-        Exp = Exp_Imputation
-    elif args.task_name == 'anomaly_detection':
-        Exp = Exp_Anomaly_Detection
-    elif args.task_name == 'classification':
-        Exp = Exp_Classification
-    elif args.task_name == 'zero_shot_forecast':
-        Exp = Exp_Zero_Shot_Forecast
-    else:
-        Exp = Exp_Long_Term_Forecast
+    # if args.task_name == 'long_term_forecast':
+    #     Exp = Exp_Long_Term_Forecast
+    # elif args.task_name == 'short_term_forecast':
+    #     Exp = Exp_Short_Term_Forecast
+    # elif args.task_name == 'imputation':
+    #     Exp = Exp_Imputation
+    # elif args.task_name == 'anomaly_detection':
+    #     Exp = Exp_Anomaly_Detection
+    # elif args.task_name == 'classification':
+    #     Exp = Exp_Classification
+    # elif args.task_name == 'zero_shot_forecast':
+    #     Exp = Exp_Zero_Shot_Forecast
+    # else:
+    #     Exp = Exp_Long_Term_Forecast
+    Exp = Exp_Long_Term_Forecast
 
     if args.is_training:
         for ii in range(args.itr):
--- scripts/long_term_custom_group.sh
+++ scripts/long_term_custom_group.sh
@@ -1,3 +1,8 @@
+
+# 自动读取 data_path 的最后一列作为 target
+DATA_PATH=adjusted_data_group_7.csv
+TARGET=$(head -1 ./datasets/$DATA_PATH | awk -F',' '{print $NF}')
+
 python3 run.py \
   --task_name long_term_forecast \
   --is_training 1 \
@@ -5,9 +10,9 @@ python3 run.py \
   --model TimesNet \
   --data custom \
   --root_path ./datasets \
-  --data_path adjusted_data_group_7.csv \
+  --data_path $DATA_PATH \
   --features M \
-  --target 10CFA10GH001XQ41 \
+  --target $TARGET \
   --freq s \
   --seq_len 12 \
   --label_len 12 \
@@ -20,24 +25,24 @@ python3 run.py \
   --batch_size 32 \
   --devices 4 \
 
-python3 run.py \
-  --task_name long_term_forecast \
-  --is_training 0 \
-  --model_id custom_group_7 \
-  --model TimesNet \
-  --data custom \
-  --root_path ./datasets \
-  --data_path adjusted_data_group_7.csv \
-  --features M \
-  --target 10CFA10GH001XQ41 \
-  --freq s \
-  --seq_len 12 \
-  --label_len 12 \
-  --pred_len 1 \
-  --enc_in 50 \
-  --dec_in 50 \
-  --c_out 50 \
-  --inverse \
-  --train_epochs 5 \
-  --batch_size 32 \
-  --devices 4 \
+# python3 run.py \
+#   --task_name long_term_forecast \
+#   --is_training 0 \
+#   --model_id custom_group_7 \
+#   --model TimesNet \
+#   --data custom \
+#   --root_path ./datasets \
+#   --data_path adjusted_data_group_7.csv \
+#   --features M \
+#   --target 10CFA10GH001XQ41 \
+#   --freq s \
+#   --seq_len 12 \
+#   --label_len 12 \
+#   --pred_len 1 \
+#   --enc_in 50 \
+#   --dec_in 50 \
+#   --c_out 50 \
+#   --inverse \
+#   --train_epochs 5 \
+#   --batch_size 32 \
+#   --devices 4 \
